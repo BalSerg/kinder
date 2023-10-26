b2e295c062b9b2f1debcf2ad0eafcf1c9906bbc2
BalSerg <motopes@yandex.ru>
2023-10-26 18:01:01 +0300

    Kinder. Markup. Part 3

--- scripts/index.js
+++ scripts/index.js
@@ -18,14 +18,23 @@ function getArrayElements(selector, parent = document) {
 }
 
 const countTemplates = 6;// Количество шаблонов
+const randomNumber = Math.round(Math.random() * 100) / 100;
 
-let elMain, elChoice, elChoiceTemplates, elEditor, elInputArea;
-
+let elMain, // Главный элемент
+    elChoice, // Блок выбора шаблонов
+    elChoiceTemplates, // Блок с самими шаблоанми
+    elEditor, // Блок редакторвания
+    elInputArea, // Поле ввода
+    valueFontStyle, // Значение для выбора стилей текста
+    elButtonsColor, // Блок цветовых кнопок
+    elBtnBack, // Кнопка Назад
+    elBtnReady, // Кнопка готово
+    isExistButtonSubmit = false // Флаг, показывающий созадана ли кнопка отправить
 
 // Функция устанавливет размеры шаблонов в блоке выбора
 function setTemplatesSizesInChoice() {
     elChoiceTemplates.childNodes.forEach((item) => {
-        item.style.width = (elChoice.offsetWidth - 16 * 3) /2 + 'px';
+        //item.style.width = (elChoice.offsetWidth - 16 * 3) /2 + 'px';
         item.style.height = (elChoice.offsetWidth - 16 * 3) /2 + 'px';
     })
 }
@@ -35,8 +44,79 @@ function setHeightMain() {
     elMain.style.height = `${screen.height}px`;
 }
 
+// Функция устанавливает высоту текстареа
 function setHeightInputArea() {
     elInputArea.style.height = `${elMain.offsetWidth - 16}px`;
+}
+
+// Функция установки полю ввода случайным образом одного из трех стилей текста
+function setRandomFontStyle() {
+    if(randomNumber < 0.33) {
+        valueFontStyle = 1;
+    }
+    else if(randomNumber > 0.33 && randomNumber < 0.66){
+        valueFontStyle = 2;
+    }
+    else {
+        valueFontStyle = 3;
+    }
+
+    elInputArea.dataset.valueFontStyle = valueFontStyle;
+    elInputArea.classList.add(`font-style-${valueFontStyle}`);
+}
+
+// Функция установки выбранного цвета в редакторе
+function chooseTextColor() {
+    const _arrButColors = elButtonsColor.childNodes;// Собрал всех потомков elButtonsColor
+    const arrButtonsColor =[]; // Массив для цветовых кнопок
+    for(let i=0; i<_arrButColors.length; i++) {// Наполнение массива цветовых кнопок
+        if(_arrButColors[i].nodeType === 1){
+            arrButtonsColor.push(_arrButColors[i]);
+        }
+    }
+    arrButtonsColor.forEach((item) => {
+        elInputArea.dataset.color = '0';
+        let oldColor;
+        if(item.nodeType === 1) {// Текущей цветовой кнопке назанчаем класс is-active
+            item.addEventListener('click', () => {
+                for(let i=0; i < arrButtonsColor.length; i++){// Удаляем у всех цветовых кнопок класс is-active
+                    arrButtonsColor[i].classList.remove('is-active');
+                }
+                oldColor = `color-${elInputArea.dataset.color}`;// Старый цвет, который поменяем на новый
+                item.classList.add('is-active');
+                elInputArea.dataset.color = item.dataset.color;
+                elInputArea.classList.replace(`${oldColor}`, `color-${elInputArea.dataset.color}`);
+            })
+        }
+    })
+}
+
+// Функция перехода назад
+function goBack() {
+    elBtnBack.addEventListener('click', () => {
+        if(isExistButtonSubmit) {
+
+        }
+        else {
+            // Скрываем редактор
+            elEditor.classList.add('is-hidden');
+
+            // Показываем выбор шаблонов
+            elChoice.classList.remove('is-hidden');
+        }
+    })
+}
+
+// Функция ввода тектса
+function enterText() {
+    elInputArea.addEventListener('input', () => {
+        if(elInputArea.value.length > 20) {
+            elBtnReady.classList.add('is-ready');
+        }
+        else {
+            elBtnReady.classList.remove('is-ready');
+        }
+    })
 }
 
 window.onload = () => {
@@ -45,6 +125,9 @@ window.onload = () => {
     elChoiceTemplates = getElement('.js-choice-templates');
     elEditor = getElement('.js-editor');
     elInputArea = getElement('.js-input-area');
+    elButtonsColor = getElement('.js-buttons-color');
+    elBtnBack = getElement('.js-btn-back');
+    elBtnReady = getElement('.js-btn-ready');
 
     setHeightMain();
 
@@ -60,6 +143,7 @@ window.onload = () => {
                 elChoice.classList.add('is-hidden');
                 elEditor.classList.remove('is-hidden');
                 elInputArea.classList.add(`template-${this.dataset.template}`);
+                setRandomFontStyle();
             }
             elDiv.addEventListener('click', goToTemplate);
         }
@@ -68,6 +152,9 @@ window.onload = () => {
     createTemplates();
     setTemplatesSizesInChoice();
     setHeightInputArea();
+    chooseTextColor();
+    goBack();
+    enterText();
 }
 
 window.onresize = () => {
