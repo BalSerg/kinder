6d607f461d3e82ade2812027ccde230ae94e61eb
BalSerg <motopes@yandex.ru>
2023-11-09 22:48:43 +0300

    Kinder. hex

--- scripts/wall.js
+++ scripts/wall.js
@@ -17,24 +17,23 @@ function getArrayElements(selector, parent = document) {
     return array;
 }
 
-let newMessage = [];
 let elWall, // Переменная для блока стены с сообщениями
     arrWallItems, // Переменная для массива сообщений
     countElements, // Переменная для количества элементов
-    defaultWidth = 400,
-    defaultHeight = 410,
+    defaultWidth = 300,
+    defaultHeight = 310,
     isDragging = false, // Переменная показывает идет ли перетаскивание
     coordLeft,
     coordTop,
     elMain, // Переменная для всего main
-    elBtnMessage; // Переменная для перехода к своему сообщению
+    elBtnMessage, // Переменная для перехода к своему сообщению
+    rightLine = [], // Массив для центральных элементов справа от 0
+    rightLineTop = []; // Массив для центральных элементов справа от 0 и всех которые вверх над ними
 
 if(window.screen.width < 768) {
     defaultWidth = 222;
     defaultHeight = 232;
 }
-
-
 
 window.onload = () => {
     // Устанавливаем высоту body
@@ -45,64 +44,107 @@ window.onload = () => {
 
     let messages = [];
 
+    let startLeft =  (window.screen.width - defaultWidth)/2;
+    let startTop =  (window.screen.height - defaultHeight)/2;
+
     fetch("https://kinder-api.brainrus.ru/list")
         .then((response) => response.json())
         .then((result) => {
             messages = result;
-
-            // Узнаем количество элементов в одном ряду и добавляем 2 для смещения
-            if(window.screen.width >= 768) {
-                countElements = Math.trunc(document.documentElement.scrollWidth / defaultWidth) + 2;
-            }
-            else {
-                countElements = 6;
-            }
-
-            // Узнаем количество линий и добавляем одну
-            let lines = Math.trunc(messages.length / countElements) + 1;
-
-            // Заполнение нового массива newMessage
-            for(let i=0; i<lines; i++) {
-                let arr = [];
-                newMessage.push(arr);
-                for(let j = 0 + countElements * i; j < countElements * (i+1); j++){
-                    if(messages[j]){
-                        newMessage[i][j] = messages[j];
-                    }
+            for(let i=0; i<messages.length; i++) {
+                let elItem;
+                elItem = document.createElement('div');
+                elItem.classList.add('wall__item');
+                elItem.classList.add(`template-${messages[i].template}`);
+                elItem.classList.add(`color-${messages[i].color}`);
+                elItem.dataset.num = messages[i].id - 1;
+                if (messages[i].isMy) {
+                    elItem.dataset.my = messages[i].isMy;
                 }
-            }
-
-            for(let i=0; i<newMessage.length; i++) {
-                // Создаем линии, их кол-во равно кол-ву массивов в newMessage
-                let elLine = document.createElement('div');
-                elLine.dataset.line = i.toString();
-
-                elWall.append(elLine); // Линии добавляем на стену
-
-                // Заполняем линии элементами исполбзуя массив newMessage
-                for (let j = 0 + countElements * i; j < countElements * (i + 1); j++) {
-                    if (typeof (newMessage[i][j]) !== 'undefined') {
-                        let elItem;
-                        elItem = document.createElement('div');
-                        elItem.classList.add('wall__item');
-                        elItem.classList.add(`template-${newMessage[i][j].template}`);
-                        elItem.classList.add(`color-${newMessage[i][j].color}`);
-                        if (newMessage[i][j].isMy) {
-                            elItem.dataset.my = newMessage[i][j].isMy;
-                        }
-                        const elPre = document.createElement('pre');
-                        elPre.textContent = `${newMessage[i][j].text}`;
-                        elPre.classList.add(`font-style-${newMessage[i][j].font_style}`);
-                        elItem.append(elPre);
-
-                        if (parseInt(elLine.dataset.line, 10) === i) {
-                            elLine.append(elItem);
-                        }
-                    }
-                }
+                const elPre = document.createElement('pre');
+                elPre.textContent = `${messages[i].text}`;
+                elPre.classList.add(`font-style-${messages[i].font_style}`);
+                elItem.append(elPre);
+                elWall.append(elItem);
             }
 
             arrWallItems = getArrayElements('.wall__item');
+            arrWallItemsNew = getArrayElements('.wall__item');
+
+            // Coordinates
+            let arrLinefrom4 = [],
+                arrLinefrom8 = [];
+            let start4 = 4,
+                start8 = 8;
+
+            // 1й в центре
+            arrWallItemsNew[0].style.left = `${startLeft}px`;
+            arrWallItemsNew[0].style.top = `${startTop}px`;
+
+            // 2й
+            arrWallItemsNew[1].style.left = `${startLeft + defaultWidth}px`;
+            arrWallItemsNew[1].style.top = `${startTop}px`;
+
+            // 3й
+            arrWallItemsNew[2].style.left = `${startLeft + defaultWidth/2}px`;
+            arrWallItemsNew[2].style.top = `${startTop + defaultHeight}px`;
+
+            //4й
+            arrWallItemsNew[3].style.left = `${startLeft - defaultWidth/2}px`;
+            arrWallItemsNew[3].style.top = `${startTop + defaultHeight}px`;
+
+            //5й
+            arrWallItemsNew[4].style.left = `${startLeft - defaultWidth}px`;
+            arrWallItemsNew[4].style.top = `${startTop}px`;
+
+            //6й
+            arrWallItemsNew[5].style.left = `${startLeft - defaultWidth/2}px`;
+            arrWallItemsNew[5].style.top = `${startTop - defaultHeight}px`;
+
+            //7й
+            arrWallItemsNew[6].style.left = `${startLeft + defaultWidth/2}px`;
+            arrWallItemsNew[6].style.top = `${startTop - defaultHeight}px`;
+
+            //8й
+            arrWallItemsNew[7].style.left = `${startLeft + 2 * defaultWidth}px`;
+            arrWallItemsNew[7].style.top = `${startTop}px`;
+
+            // от 4го элемента и влево
+            for(let i = 2; i< arrWallItemsNew.length; i++){
+                start4 = (start4 + (6 * i) - 2);
+                arrLinefrom4.push(start4);
+            }
+            for (let i =0; i < arrLinefrom4.length; i++){
+                if(arrWallItemsNew[arrLinefrom4[i]]){
+                    arrWallItemsNew[arrLinefrom4[i]].style.left = `${startLeft - defaultWidth * (i + 2)}px`;
+                    arrWallItemsNew[arrLinefrom4[i]].style.top = `${startTop}px`;
+                }
+            }
+
+            // от 8го элемента и вправо
+            for(let i = 2; i< arrWallItemsNew.length; i++){
+                start8 = (start8 + (6 * i) + 1);
+                arrLinefrom8.push(start8);
+            }
+            for (let i =0; i < arrLinefrom8.length; i++){
+                if(arrWallItemsNew[arrLinefrom8[i]]){
+                    arrWallItemsNew[arrLinefrom8[i]].style.left = `${startLeft + defaultWidth * (i + 3)}px`;
+                    arrWallItemsNew[arrLinefrom8[i]].style.top = `${startTop}px`;
+                }
+            }
+
+            rightLine = [0, 1, 8, ...arrLinefrom8];
+
+            //
+            for(let i=0; i<rightLine.length; i++) {
+                let start = rightLine[i];
+                for(let j=0; j<rightLine.length; j++) {
+                    start = start + 6*j;
+                    rightLineTop.push(start);
+                }
+            }
+
+            console.log(rightLine);
 
             elWall.style.width = `${defaultWidth * countElements}px`;
             elWall.style.left = `${(document.body.offsetWidth - elWall.offsetWidth)/2}px`;
